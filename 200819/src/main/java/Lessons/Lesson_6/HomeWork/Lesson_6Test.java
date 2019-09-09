6feb945efaaf92242e322948e089a5df15cb8d5c
Dmitriy <larkingit@yandex.ru>
2019-09-09 13:59:28 +0700

    Написал параметризированный тест на task1()

--- 200819/src/main/java/Lessons/Lesson_6/HomeWork/Lesson_6Test.java
+++ 200819/src/main/java/Lessons/Lesson_6/HomeWork/Lesson_6Test.java
@@ -1,6 +1,7 @@
 package Lessons.Lesson_6.HomeWork;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,23 +10,33 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
-
 
 @RunWith(Parameterized.class)
 public class Lesson_6Test {
     private Lesson_6 homework;
-    private Integer[] array;
+    private Integer[] test;
+    private Integer[] expected;
+
+
+    public Lesson_6Test(Integer[] test, Integer[] expected) {
+        this.test = test;
+        this.expected = expected;
+    }
+
+
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
-        return Arrays.asList(
-        );
+        return Arrays.asList(new Object[][] {
+                {new Integer[]{1,1,1,1,4,1,7}, new Integer[]{1,7}},
+                {new Integer[]{1,4,1,1,1,7}, new Integer[]{1,1,1,7}},
+                {new Integer[]{4,1,1,7,7,1,7}, new Integer[]{1,1,7,7,1,7}},
+                {new Integer[]{1,1,4,4,4,4,4,1}, new Integer[]{1}}
+        });
     }
 
     @Before
     public void init() {
         homework = new Lesson_6();
-        array = new Integer[] {1,1,4,1,7};
     }
 
     @After
@@ -35,10 +46,11 @@ public class Lesson_6Test {
 
     @Test
     public void task1() {
-        assertEquals("[1, 7]", Arrays.toString(homework.task1(array)));
+        Assert.assertArrayEquals(expected, homework.task1(test));
     }
 
     @Test
     public void task2() {
+
     }
 }
