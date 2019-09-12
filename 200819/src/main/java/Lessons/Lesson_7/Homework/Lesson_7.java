cfa642af13aeca9272690c56dd75eea1cca6997b
Dmitriy <larkingit@yandex.ru>
2019-09-12 13:47:35 +0700

    выполнил домашнее заданее

--- 200819/src/main/java/Lessons/Lesson_7/Homework/Lesson_7.java
+++ 200819/src/main/java/Lessons/Lesson_7/Homework/Lesson_7.java
@@ -1,6 +1,14 @@
 package Lessons.Lesson_7.Homework;
 
+import Lessons.Lesson_7.Homework.Annotations.AfterSuite;
+import Lessons.Lesson_7.Homework.Annotations.BeforeSuite;
 import Lessons.Lesson_7.Homework.Annotations.Test;
+import Lessons.Lesson_7.Homework.TestClasses.MyClass;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 /*
 Создать класс, который может выполнять «тесты». В качестве тестов выступают классы с наборами методов с аннотациями @Test.
@@ -12,8 +20,57 @@ import Lessons.Lesson_7.Homework.Annotations.Test;
 должны присутствовать в единственном экземпляре, иначе необходимо бросить RuntimeException при запуске «тестирования».
  */
 public class Lesson_7 {
-    @Test
-    public void markedMethod() {
-        System.out.println("Java");
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        try {
+            start(myClass);
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void start(String klassName) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        start(Class.forName(klassName));
+    }
+    public static void start(Object klass) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        startTests(klass.getClass());
+    }
+
+    private static Comparator<Method> methodPriorityComparator = (firstMethod, secondMethod) -> {
+        Integer firstMethodPriority = firstMethod.getAnnotation(Test.class).priority();
+        Integer secondMethodPriority = secondMethod.getAnnotation(Test.class).priority();
+        if (firstMethodPriority.compareTo(secondMethodPriority) == 0)
+            return firstMethod.getName().compareTo(secondMethod.getName());
+        return firstMethodPriority.compareTo(secondMethodPriority);
+    };
+
+    private static <T> void startTests(Class<T> klass) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        if (Collections.frequency(new ArrayList<Annotation>(Arrays.asList(klass.getAnnotations())), BeforeSuite.class) > 1)
+            throw new RuntimeException("@BeforeSuite > 1");
+        else {
+            final T klassInstance;
+            final Method[] beforeAfter = {null, null};
+            Set<Method> testMethods = new TreeSet<>(methodPriorityComparator);
+            klassInstance = klass.newInstance();
+            new ArrayList<Method>(Arrays.asList(klass.getDeclaredMethods())).forEach(method -> {
+                method.setAccessible(true);
+                if (method.isAnnotationPresent(BeforeSuite.class))
+                    beforeAfter[0] = method;
+                else if (method.isAnnotationPresent(AfterSuite.class))
+                    beforeAfter[1] = method;
+                else if (method.isAnnotationPresent(Test.class))
+                    testMethods.add(method);
+            });
+            if (beforeAfter[0] !=null) beforeAfter[0].invoke(klassInstance);
+            testMethods.forEach(method -> {
+                try {
+                    method.invoke(klassInstance);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            });
+            if (beforeAfter[1] != null)
+                beforeAfter[1].invoke(klassInstance);
+        }
     }
 }
